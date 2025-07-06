8a3f6434a390f514e1c665b2089bae0c28432bd7
Your Name <you@example.com>
2025-07-06 17:31:02 -0400

    Data check

--- main.py
+++ main.py
@@ -53,23 +53,30 @@ def go(config: DictConfig):
         if "basic_cleaning" in active_steps:
             # Clean file
             _ = mlflow.run(
-                     os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
-                     "main",
-                     parameters={
-                         "input_artifact": "sample.csv:latest",
-                         "output_artifact": "clean_sample.csv",
-                         "output_type": "clean_sample",
-                         "output_description": "Basic data cleaning",
-                         "min_price": config['etl']['min_price'],
-                         "max_price": config['etl']['max_price']
-                     },
-                 )
+                    os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                    "main",
+                    parameters={
+                        "input_artifact": "sample.csv:latest",
+                        "output_artifact": "clean_sample.csv",
+                        "output_type": "clean_sample",
+                        "output_description": "Basic data cleaning",
+                        "min_price": config['etl']['min_price'],
+                        "max_price": config['etl']['max_price']
+                    },
+                )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                    os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                    "main",
+                    parameters={
+                        "csv": "clean_sample.csv:latest",
+                        "ref": "clean_sample.csv:reference",
+                        "kl_threshold": config["data_check"]["kl_threshold"],
+                        "min_price": config['etl']['min_price'],
+                        "max_price": config['etl']['max_price']
+                    },
+            )
 
         if "data_split" in active_steps:
             ##################
